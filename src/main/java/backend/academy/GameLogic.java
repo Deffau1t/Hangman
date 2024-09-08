4627a46b659169ef0f70bcb0db2f404f63b47546
Dave M <default8@bk.ru>
2024-09-08 16:25:24 +0300

    Few corrections + primitive realisation of game

--- src/main/java/backend/academy/GameLogic.java
+++ src/main/java/backend/academy/GameLogic.java
@@ -1,14 +1,16 @@
 package backend.academy;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GameLogic {
-    private String difficulty = "medium";
+    private int difficulty = 2;
     private String category;
     private int attempts;
     private String word;
 
-    public GameLogic(String difficulty, String category) {
+    public GameLogic(int difficulty, String category) {
         this.difficulty = difficulty;
         this.category = category;
     }
@@ -20,28 +22,49 @@ public class GameLogic {
         return this.category;
     }
 
-    public String[] Word_list() {
+    public int getAttempts() {
+        return this.attempts;
+    }
+
+    //отбор подходящего списка слов из начального словаря
+    public String[] word_list() {
         GameData dict = new GameData();
-        if (this.difficulty.equals("hard")) {
-            this.attempts = 3;
-            return dict.getDictionary(this.category).get("hard");
-        }
-        if (this.difficulty.equals("medium")) {
-            this.attempts = 4;
-            return dict.getDictionary(this.category).get("medium");
-        }
-        if (this.difficulty.equals("easy")) {
-            this.attempts = 5;
-            return dict.getDictionary(this.category).get("easy");
+        switch (this.difficulty) {
+            case 3 -> {
+                this.attempts = 3;
+                return dict.getDictionary(this.category).get("hard");
+            }
+            case 2 -> {
+                this.attempts = 4;
+                return dict.getDictionary(this.category).get("medium");
+            }
+            case 1 -> {
+                this.attempts = 5;
+                return dict.getDictionary(this.category).get("easy");
+            }
         }
         return dict.getDictionary(this.category).get("medium");
     }
 
-    public String GetWord() {
-        String [] chosen_words = Word_list();
+    //метод для получения случайного слова из списка по заданным параметрам
+    public String getWord() {
+        String [] chosen_words = word_list();
         Random random_ind = new Random();
         int index_word = random_ind.nextInt(chosen_words.length);
         return chosen_words[index_word];
+    }
+
+    //метод для получения индекса буквы в слове
+    public List<Integer> foundLetter(char letter, String answer) {
+        answer = answer.toLowerCase();
+        letter = Character.toLowerCase(letter);
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < answer.length(); i ++) {
+            if (answer.charAt(i) == letter) {
+                res.add(i);
+            }
+        }
+        return res;
     }
 
 }
